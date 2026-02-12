f3cfacc3ff44f5b6b43cf6e83dc9303cb1ffbbc4
natureenook <naaturee.nook@gmail.com>
2026-02-12 20:32:20 +0400

    Fixed routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Path from "./Components/Path/Path"
+import { HashRouter as Router } from "react-router-dom";
+import Path from "./Components/Path/Path";
 
 function App() {
   return (
